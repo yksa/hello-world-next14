a18e2c7f082fcc055cbb6bf554879955a7e54b69
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-03-21 11:11:58 +0630

    Routing Metadata

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,7 +1,26 @@
-export default function ProductDetail({
-  params,
-}: {
+import { Metadata } from "next";
+
+type Props = {
   params: { productId: string };
-}) {
+};
+
+export const generateMetadata = ({ params }: Props): Metadata => {
+  return { title: `Product ${params.productId}` };
+};
+
+// async function
+// export const generateMetadata = async ({
+//   params,
+// }: Props): Promise<Metadata> => {
+//   const title = await new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(`iPhone ${params.productId}`);
+//     }, 100);
+//   });
+//   console.log({ title });
+//   return { title: `Product ${title}` };
+// };
+
+export default function ProductDetail({ params }: Props) {
   return <h1>Details about product {params.productId}</h1>;
 }
