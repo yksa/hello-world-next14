1f0c70f544629cef1fc9d6ffc6751a40dafa5836
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-03-28 10:15:06 +0630

    title Metadata

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next";
+
+// Parent title template will be ignored
+// export const metadata: Metadata = { title: { absolute: "Blog" } };
+export const metadata: Metadata = { title: "Blog" };
+
 export default function Blog() {
   return <h1>My blog</h1>;
 }
